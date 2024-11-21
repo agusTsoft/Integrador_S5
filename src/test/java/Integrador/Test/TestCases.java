808cf159709bc67d78db49433afebac4818c80ee
Agus <agustin.villa@tsoftglobal.com>
2024-11-21 15:40:41 -0300

    actualizacion

--- src/test/java/Integrador/Test/TestCases.java
+++ src/test/java/Integrador/Test/TestCases.java
@@ -1,18 +1,53 @@
 package Integrador.Test;
 
+import Integrador.Pages.HomePage;
+import Integrador.Utils.BaseClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.FileNotFoundException;
+import java.util.concurrent.TimeUnit;
 
-public class TC00 {
+
+public class TestCases {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private HomePage homePage;
+    private BaseClass baseClass;
+    private static String urlDriver = "C:\\IntegradorS5\\src\\test\\resources\\Drivers\\chromedriver.exe";
+    private static String property = "webdriver.chrome.driver";
+    private static String browser = "chrome";
+
     @Test
-    public void TC001() throws FileNotFoundException {
+    public void TC001_createAccountBtn() throws InterruptedException {
+        homePage.register();
+        assertTrue(homePage.buscarWebElement(By.xpath("//h4[text()]")).getText().contains("Felicidades"));
     }
+
 
     @BeforeEach
     public void preConditions() throws FileNotFoundException {
+        baseClass = new BaseClass(driver, wait);
+        driver = baseClass.conexionDriver(browser, urlDriver, property);
+        wait = new WebDriverWait(driver,20);
+        homePage = new HomePage(driver, wait);
+        driver.get("https://buenosaires.gob.ar/inicio/");
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(45, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
     }
-    
+
+    @AfterEach
+    public void postConditions(){
+        //driver.close()
+    }
+
 }
--- src/test/java/Integrador/Utils/DataDriven.java
+++ src/test/java/Integrador/Utils/DataDriven.java
@@ -1,4 +1,82 @@
 package Integrador.Utils;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class DataDriven {
+    public ArrayList<String> conexionDataSet(String TestCase) {
+
+        ArrayList<String> data = new ArrayList<>();
+
+        FileInputStream file = null;
+        try {
+            file = new FileInputStream("C:\\IntegradorS5\\src\\test\\resources\\Data\\DataSet.xlsx");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        XSSFWorkbook excel = null;
+        try{
+            excel = new XSSFWorkbook(file);
+        }catch (IOException e){
+            System.out.println("Archivo no encontrado");
+        }
+        int cantHojas = 0;
+        if (excel != null) {
+            cantHojas = excel.getNumberOfSheets();
+        }else{
+            System.out.println("'Excel' es null");
+        }
+
+        for (int i = 0; i<cantHojas; i++){
+            if (excel.getSheetName(i).equalsIgnoreCase("DatosCP")){
+                XSSFSheet Hoja = excel.getSheetAt(i);
+
+                Iterator<Row> filas = Hoja.iterator();
+
+                Row primeraFila = filas.next();
+
+                Iterator<Cell> celda =  primeraFila.cellIterator();
+
+                int k = 0;
+                int columna = 0;
+
+                while(celda.hasNext()){
+                    Cell celdaSeleccionada = celda.next();
+                    if(celdaSeleccionada.getStringCellValue().equalsIgnoreCase("CasosDePrueba")){
+                        columna = k;
+                    }
+                    k++;
+                }
+                while (filas.hasNext()){
+                    Row r = filas.next();
+
+                    if (r.getCell(columna).getStringCellValue().equalsIgnoreCase(TestCase)){
+                        Iterator<Cell> cv = r.cellIterator();
+
+                        while(cv.hasNext()){
+                            Cell cell = cv.next();
+
+                            if (cell.getCellType() == CellType.STRING){
+                                data.add(cell.getStringCellValue());
+                            }else if(cell.getCellType() == CellType.NUMERIC){
+                                data.add(NumberToTextConverter.toText(cell.getNumericCellValue()));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return data;
+    }
 }
