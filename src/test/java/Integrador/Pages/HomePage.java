5f6cf1fdc503542c6de2806f0fe69758c5c7a0c3
Agus <agustin.villa@tsoftglobal.com>
2024-11-22 16:49:36 -0300

    Desglose y creacion de Testcases variados

--- src/test/java/Integrador/Pages/HomePage.java
+++ src/test/java/Integrador/Pages/HomePage.java
@@ -1,70 +1,29 @@
 package Integrador.Pages;
 
 import Integrador.Utils.BaseClass;
-import Integrador.Utils.DataDriven;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.ArrayList;
 
 public class HomePage extends BaseClass {
     By byTramitesYServicios = By.xpath("//a[@href='/inicio/tramites-y-servicios/45']");//a[text()='Crear cuenta'
     By byBibliotecas = By.xpath("//a[@href='/tramites-y-servicios/49']");
     By byAsosiarse = By.xpath("//h3[text()='Asociarse a la red de Bibliotecas públicas de la Ciudad']");
     By byLinkRegister = By.xpath("//a[@href='https://portalinscripciones.scp.buenosaires.gob.ar/#/actividad/8711']");
-    By byInscriptionBtn = By.xpath("//button[text()='Inscribite']");
-    By byName = By.xpath("//input[@id='field-11453']");
-    By byLastName = By.xpath("//input[@id='field-11454']");
-    By byDocType = By.xpath("//select[@id='field-11455']");
-    By byDocNum = By.xpath("//input[@id='field-11463']");
-    By byGender = By.xpath("//select[@id='field-11456']");
-    By byBirthDate = By.xpath("//input[@id='field-11457']");
-    By byEmailField = By.xpath("//input[@id='field-11459']");
-    By byConfirm = By.xpath("//input[@id='field-11458']");
-    By byProvince = By.xpath("//select[@id='field-11460']");
-    By byLocality = By.xpath("//select[@id='field-11462']");
-    By byAdress = By.xpath("//input[@id='field-11461']");
-    By byPhoneNumer = By.xpath("//input[@id='field-11465']");
-    By byNationality = By.xpath("//select[@id='field-11464']");
-    By byComuna = By.xpath("//input[@id='field-11466']");
-    By bySendBtn = By.xpath("//button[text()='Enviar']");
-
 
     public HomePage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
 
-    public void register() throws InterruptedException {
-        DataDriven colector = new DataDriven();
-        ArrayList<String> registerData = colector.conexionDataSet("CP001");
-
+    public void goToRegisterPage() throws InterruptedException {
         wait.until(ExpectedConditions.presenceOfElementLocated(byTramitesYServicios)).click();
         wait.until(ExpectedConditions.presenceOfElementLocated(byBibliotecas)).click();
         Thread.sleep(1000);
         wait.until(ExpectedConditions.presenceOfElementLocated(byAsosiarse)).click();
         wait.until(ExpectedConditions.presenceOfElementLocated(byLinkRegister)).click();
-        wait.until(ExpectedConditions.presenceOfElementLocated(byInscriptionBtn)).click();
-        wait.until(ExpectedConditions.presenceOfElementLocated(byName));
-        if (buscarWebElement(byName).isDisplayed()){
-            buscarWebElement(byName).sendKeys(registerData.get(1));
-            buscarWebElement(byLastName).sendKeys(registerData.get(2));
-            seleccionarDDLPorTexto(buscarWebElement(byDocType), registerData.get(3));
-            buscarWebElement(byDocNum).sendKeys(registerData.get(4));
-            seleccionarDDLPorTexto(buscarWebElement(byGender), registerData.get(5));
-            buscarWebElement(byBirthDate).sendKeys(registerData.get(6));
-            buscarWebElement(byEmailField).sendKeys(registerData.get(7));
-            buscarWebElement(byConfirm).sendKeys(registerData.get(8));
-            seleccionarDDLPorTexto(buscarWebElement(byProvince), registerData.get(9));
-            buscarWebElement(byLocality).sendKeys(registerData.get(10));
-            buscarWebElement(byAdress).sendKeys(registerData.get(11));
-            buscarWebElement(byPhoneNumer).sendKeys(registerData.get(12));
-            seleccionarDDLPorTexto(buscarWebElement(byNationality), registerData.get(13));
-            buscarWebElement(byComuna).sendKeys(registerData.get(14));
-            click(buscarWebElement(bySendBtn));
-        }
-    }
 
+    }
 }
+
--- src/test/java/Integrador/Test/TestCases.java
+++ src/test/java/Integrador/Test/TestCases.java
@@ -1,6 +1,7 @@
 package Integrador.Test;
 
 import Integrador.Pages.HomePage;
+import Integrador.Pages.RegisterPage;
 import Integrador.Utils.BaseClass;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -20,17 +21,46 @@ public class TestCases {
     private WebDriver driver;
     private WebDriverWait wait;
     private HomePage homePage;
+    private RegisterPage registerPage;
     private BaseClass baseClass;
     private static String urlDriver = "C:\\IntegradorS5\\src\\test\\resources\\Drivers\\chromedriver.exe";
     private static String property = "webdriver.chrome.driver";
     private static String browser = "chrome";
 
+
     @Test
-    public void TC001_createAccountBtn() throws InterruptedException {
-        homePage.register();
+    public void TC001_goToRegisterPage() throws InterruptedException {
+        driver.get("https://buenosaires.gob.ar/inicio/");
+        homePage.goToRegisterPage();
+    }
+
+    @Test
+    public void TC002_SendEmpty() throws InterruptedException{
+        driver.get("https://portalinscripciones.scp.buenosaires.gob.ar/#/actividad/8711");
+        registerPage.SendForm();
+        assertEquals("https://portalinscripciones.scp.buenosaires.gob.ar/#/actividad/8711", registerPage.getDriver().getPageSource());
+    }
+
+    @Test
+    public void TC003_registerOK() throws InterruptedException {
+        driver.get("https://portalinscripciones.scp.buenosaires.gob.ar/#/actividad/8711");
+        registerPage.fill("CP002");
         assertTrue(homePage.buscarWebElement(By.xpath("//h4[text()]")).getText().contains("Felicidades"));
     }
 
+    @Test
+    public void TC004_RegisterNOK() throws InterruptedException{
+        driver.get("https://portalinscripciones.scp.buenosaires.gob.ar/#/actividad/8711");
+        registerPage.fill("CP003");
+        assertTrue(homePage.buscarWebElement(By.id("field-11463-error")).getText().contains("Ya estás registrado/a en esta actividad"));
+    }
+
+    @Test
+    public void TC005_AgeBelow18() throws InterruptedException{
+        driver.get("https://portalinscripciones.scp.buenosaires.gob.ar/#/actividad/8711");
+        registerPage.fill("CP005");
+        assertEquals("Sección Adulto/Tutor Responsable", registerPage.buscarWebElement(By.xpath("//h2[text()='Sección Adulto/Tutor Responsable']")).getText());
+    }
 
     @BeforeEach
     public void preConditions() throws FileNotFoundException {
@@ -38,7 +68,7 @@ public class TestCases {
         driver = baseClass.conexionDriver(browser, urlDriver, property);
         wait = new WebDriverWait(driver,20);
         homePage = new HomePage(driver, wait);
-        driver.get("https://buenosaires.gob.ar/inicio/");
+        registerPage  = new RegisterPage(driver, wait);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(45, TimeUnit.SECONDS);
